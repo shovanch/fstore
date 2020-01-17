ce7cdb63e38055d3f41062d02d68e1746736097e
Shovan Chatterjee <hello@shovanch.com>
2020-01-17 23:54:15 +0530

    Fix classname jsx error

--- src/components/withRouterLoading.js
+++ src/components/withRouterLoading.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const withRouterLoading = () => (
   <main>
-    <div class="spinner">
-      <div class="double-bounce1"></div>
-      <div class="double-bounce2"></div>
+    <div className="spinner">
+      <div className="double-bounce1" />
+      <div className="double-bounce2" />
     </div>
   </main>
 );
