795d132448e1118eed692268cbba78e2b21d5ca9
Shovan Chatterjee <hello@shovanch.com>
2020-01-17 23:09:13 +0530

    Static react Collectionpage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 /* eslint-disable react/prefer-stateless-function */
 import React, { Component } from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+
 import Header from "components/Header";
 import HomePage from "pages/HomePage";
+import CollectionPage from "pages/CollectionPage";
 
 class App extends Component {
   render() {
     return (
       <div className="container">
         <Header />
-        <HomePage />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/collection" component={CollectionPage} />
+        </Switch>
       </div>
     );
   }
---
+++ src/components/CollectionHeader.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const CollectionHeader = ({ title }) => (
+  <div className="product-hero collections__link background-img sale__background">
+    <h1 className="collections__link-text product-hero__text">{title}</h1>
+  </div>
+);
+
+export default CollectionHeader;
